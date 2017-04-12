db48ae95476a04a0fb8beeb0cdf28fe21fef8969
Marin Atanasov <tyxla@abv.bg>
2017-04-12 21:57:04 +0300

    Site Settings: Support for private/hidden sites in Sitemap card

--- client/my-sites/site-settings/traffic/main.jsx
+++ client/my-sites/site-settings/traffic/main.jsx
@@ -79,6 +79,7 @@ const SiteSettingsTraffic = ( {
 		<Sitemaps
 			isSavingSettings={ isSavingSettings }
 			isRequestingSettings={ isRequestingSettings }
+			fields={ fields }
 		/>
 	</Main>
 );
@@ -115,6 +116,7 @@ const getFormSettings = partialRight( pick, [
 	'jetpack_relatedposts_show_thumbnails',
 	'amp_is_supported',
 	'amp_is_enabled',
+	'blog_public',
 ] );
 
 export default flowRight(
