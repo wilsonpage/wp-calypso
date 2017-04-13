bc69c07b60a9ff80b54bedbd842eb3f209e4eaee
Kevin Killingsworth <kevin@redfenix.com>
2017-04-13 18:53:48 -0500

    WooCommerce: Add product details to form
    This adds product details and populates them with the current product
    edit state.

--- client/extensions/woocommerce/app/products/product-form.js
+++ client/extensions/woocommerce/app/products/product-form.js
@@ -7,6 +7,10 @@ import i18n from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
+import FormLabel from 'components/forms/form-label';
+import FormTextArea from 'components/forms/form-textarea';
+import FormTextInput from 'components/forms/form-text-input';
 import FoldableCard from 'components/foldable-card';
 import ProductVariationTypesForm from './product-variation-types-form';
 import FormToggle from 'components/forms/form-toggle';
@@ -15,10 +19,12 @@ export default class ProductForm extends Component {
 
 	static propTypes = {
 		product: PropTypes.shape( {
-			id: PropTypes.number.isRequired,
-			name: PropTypes.string.isRequired,
+			id: PropTypes.isRequired,
 			type: PropTypes.string.isRequired,
-		} )
+			name: PropTypes.string,
+		} ),
+		editProduct: PropTypes.func.isRequired,
+		editProductAttribute: PropTypes.func.isRequired,
 	};
 
 	constructor( props ) {
@@ -29,6 +35,9 @@ export default class ProductForm extends Component {
 		};
 
 		this.handleToggle = this.handleToggle.bind( this );
+		this.setName = this.setName.bind( this );
+		this.setDescription = this.setDescription.bind( this );
+		this.toggleFeatured = this.toggleFeatured.bind( this );
 	}
 
 	handleToggle() {
@@ -37,8 +46,25 @@ export default class ProductForm extends Component {
 		} ) );
 	}
 
+	setName( e ) {
+		const { product, editProduct } = this.props;
+		editProduct( product, { name: e.target.value } );
+	}
+
+	setDescription( e ) {
+		const { product, editProduct } = this.props;
+		editProduct( product, { description: e.target.value } );
+	}
+
+	toggleFeatured() {
+		const { product, editProduct } = this.props;
+		editProduct( product, { featured: ! product.featured } );
+	}
+
 	render() {
 		const { product } = this.props;
+		const __ = i18n.translate;
+
 		const variationToggleDescription = i18n.translate(
 			'%(productName)s has variations, for example size and color.', {
 				args: {
@@ -46,20 +72,57 @@ export default class ProductForm extends Component {
 				}
 			}
 		);
+
 		return (
-			<FoldableCard
-				icon=""
-				expanded={ true }
-				className="product-variations"
-				header={ ( <FormToggle onChange={ this.handleToggle } checked={ this.state.isVariation }>
-				{variationToggleDescription}
-				</FormToggle>
-				) }
-			>
-				{ this.state.isVariation && (
-					<ProductVariationTypesForm />
-				) }
-			</FoldableCard>
+			<div>
+				<Card
+					className="products__product-form-details"
+				>
+					<div>
+						<FormLabel>
+							{ __( 'Featured' ) }
+							<FormToggle
+								onChange={ this.toggleFeatured }
+								checked={ product.featured }
+							/>
+						</FormLabel>
+					</div>
+					<div>
+						<FormLabel>
+							<div>{ __( 'Product images' ) }</div>
+							<div>[ Big Image Box ]</div>
+							<div>{ __( 'For best results, upload photos larger than 1000x1000px' ) }</div>
+						</FormLabel>
+					</div>
+					<div>
+						<FormLabel>
+							<span>{ __( 'Product name' ) }</span>
+							<FormTextInput value={ product.name || '' } onChange={ this.setName } />
+						</FormLabel>
+						<FormLabel>
+							<span>{ __( 'SKU' ) }: { product.sku || __( '-' ) } </span>
+						</FormLabel>
+						<FormLabel>
+							<span>{ __( 'Description' ) }</span>
+							<FormTextArea value={ product.description || '' } onChange={ this.setDescription } />
+						</FormLabel>
+					</div>
+				</Card>
+
+				<FoldableCard
+					icon=""
+					expanded={ true }
+					className="product-variations"
+					header={ ( <FormToggle onChange={ this.handleToggle } checked={ this.state.isVariation }>
+					{variationToggleDescription}
+					</FormToggle>
+					) }
+				>
+					{ this.state.isVariation && (
+						<ProductVariationTypesForm />
+					) }
+				</FoldableCard>
+			</div>
 		);
 	}
 
