6cdd4819ac19a5128e5d33c40ebb2228653513c4
Donna Peplinskie <donnapep@gmail.com>
2017-04-19 11:34:40 -0400

    Add selectors for determining the save status

--- client/extensions/wp-super-cache/state/selectors.js
+++ client/extensions/wp-super-cache/state/selectors.js
@@ -17,6 +17,30 @@ export function isRequestingSettings( reduxState, siteId ) {
 }
 
 /**
+ * Returns true if we are saving settings for the specified site ID, false otherwise.
+ *
+ * @param  {Object}  reduxState Global state tree
+ * @param  {Number}  siteId Site ID
+ * @return {Boolean} Whether settings are being saved
+ */
+export function isSavingSettings( reduxState, siteId ) {
+	const state = reduxState.extensions.wpSuperCache;
+
+	return state ? get( state.saveStatus, [ siteId, 'saving' ], false ) : false;
+}
+
+/**
+ * Returns true if the settings save request was successful.
+ *
+ * @param  {Object}  reduxState Global state tree
+ * @param  {Number}  siteId Site ID
+ * @return {Boolean} Whether settings were saved successfully
+ */
+export function isSettingsSaveSuccessful( reduxState, siteId ) {
+	return getSettingsSaveStatus( reduxState, siteId ) === 'success';
+}
+
+/**
  * Returns the settings for the specified site ID.
  *
  * @param  {Object} reduxState Global state tree
@@ -27,4 +51,30 @@ export function getSettings( reduxState, siteId ) {
 	const state = reduxState.extensions.wpSuperCache;
 
 	return state ? get( state.items, [ siteId ], null ) : null;
+}
+
+/**
+ * Returns the status of the last settings save request.
+ *
+ * @param  {Object}  reduxState Global state tree
+ * @param  {Number}  siteId Site ID
+ * @return {String}  Save request status (pending, success or error)
+ */
+export function getSettingsSaveStatus( reduxState, siteId ) {
+	const state = reduxState.extensions.wpSuperCache;
+
+	return state ? get( state.saveStatus, [ siteId, 'status' ], null ) : null;
+}
+
+/**
+ * Returns the error returned by the last settings save request.
+ *
+ * @param  {Object}  reduxState Global state tree
+ * @param  {Number}  siteId Site ID
+ * @return {String}  Request error
+ */
+export function getSettingsSaveError( reduxState, siteId ) {
+	const state = reduxState.extensions.wpSuperCache;
+
+	return state ? get( state.saveStatus, [ siteId, 'error' ], false ) : false;
 }
